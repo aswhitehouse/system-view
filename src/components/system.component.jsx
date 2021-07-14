23653b432a941e72518ee5296fcf9e38b88f8dfd
Andrew Whitehouse <andrewswhitehouse@gmail.com>
2021-07-14 13:17:30 +1000

    remove fs and get values from an api call

--- src/components/system.component.jsx
+++ src/components/system.component.jsx
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Fragment} from 'react';
 import ReactFlow from 'react-flow-renderer';
 import SystemNode from '../models/SystemNode';
 import EdgeDetail from '../models/EdgeDetail';
 import Data from '../models/Data';
 import Position from '../models/Position';
+import {properties} from '../resources/properties';
 
-const elements = [
-    new SystemNode('1', 'input', new Data('Node One'), new Position(250, 5)),
-    new SystemNode('2', 'input', new Data('Node Two'), new Position(500, 200)),
-    new EdgeDetail('e1', '1', '2', true)
-];
 
-const SystemMap = () => {
-    return (
-        <Fragment>
-            <ReactFlow elements={elements}
-                style={
-                    {
-                        width: '100%',
-                        height: '90vh'
+class SystemMap extends Component {
+    constructor() {
+        super();
+        this.state = {
+            elements: []
+        };
+    }
+    componentDidMount() {
+        fetch(properties.logApi).then(res => {
+            return res.json()
+        }).then(elements => {
+            this.setState({elements})
+        })
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <ReactFlow elements={
+                        [
+                            new SystemNode('1', 'input', new Data(this.state.elements.id1), new Position(250, 5)),
+                            new SystemNode('2', 'input', new Data(this.state.elements.id2), new Position(500, 200)),
+                            new EdgeDetail('e1', '1', '2', true)
+                        ]
                     }
-                }/>
-        </Fragment>
-    )
+                    style={
+                        {
+                            width: '100%',
+                            height: '90vh'
+                        }
+                    }/>
+            </Fragment>
+        );
+    }
 }
 
 export default SystemMap;
---
+++ src/resources/properties.js
@@ -0,0 +1,3 @@
+export const properties = {
+    logApi:"http://localhost:3005"
+}
